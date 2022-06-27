e3cc9a72de9c8cb1021b326ebb317d78c8c37b62
icemanparachute <acfmichelsen@gmail.com>
2022-06-27 09:56:17 -0400

    major commit

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    version: '0.8.13',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    }
+  }, 
+};
