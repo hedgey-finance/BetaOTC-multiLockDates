877e8e51ab2479a3416082a5d461634611298560
icemanparachute <acfmichelsen@gmail.com>
2022-07-11 13:43:26 -0400

    update one

--- contracts/test/WLToken.sol
+++ contracts/test/WLToken.sol
@@ -8,20 +8,21 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 /// @notice this is a specific token that is used to airdrop to a whitelist group of ppl
 /// this token can be burned or minted to users, but it cannot be transferred
 contract WLToken is ERC20, Ownable {
+    uint256 private constant one = 1 * (10 ** 18);
     constructor(string memory tokenName, string memory tokenSymbol) ERC20(tokenName, tokenSymbol) {}
 
     /// @notice function to airdrop tokens to the whitelisted individuals, only sends 1 token each, as that's all they need
     /// @dev runs through the array of the whitelist and calls the internal mint function sending each one 1 token
     function list(address[] memory whiteList) external onlyOwner {
         for (uint256 i; i < whiteList.length; i++) {
-            _mint(whiteList[i], 1);
+            _mint(whiteList[i], one);
         }
     }
 
     /// @notice this function just burns the tokens of each user the owner wants to delist
     function deList(address[] memory blackList) external onlyOwner {
         for (uint256 i; i < blackList.length; i++) {
-            _burn(blackList[i], 1);
+            _burn(blackList[i], one);
         }
     }
 
